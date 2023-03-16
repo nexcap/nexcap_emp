478be178fda964edd2e0590b46e78867ab2c93ae
krishnakumar003 <86408736+krishnakumar003@users.noreply.github.com>
2023-03-16 23:58:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://0.0.0.0:27017/nexcap", {useNewUrlParser: true});
+
+
+const EmpSchema = {
+        Emp_ID : String,
+        FirstName : String,
+        LastName : String,
+        Email_ID : String,
+        DOB : String,
+        Blood : String,
+        Sex : String,
+        Age : Number,
+        phn_no : Number,
+        Image: String   
+ 
+}
+
+
+const Emp1 = mongoose.model("emp_nexcap", EmpSchema);
+
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+var key = makeid(10);
+var keyver = key;
+
+
+app.get("/",function(req,res){
+    res.render("index")
+})
+
+
+app.get("/terms",function(req,res){
+    res.render("terms")
+})
+
+app.get("/privacy",function(req,res){
+    res.render("privacy")
+})
+
+
+
+
+
+
+
+
+
+app.get("/:emp_ID", function(req,res){
+    
+    if (key === keyver){
+        Emp1.findOne({Emp_ID: req.params.emp_ID},function(err , found){
+            if (err){
+                res.send("Please use QR")
+            } else{
+                if (found === null){
+                    res.send("<h1>Please use QR</h1>")
+                }else{
+                    console.log(found.Image)
+                    res.render("index",
+                {
+                    
+                    EmpID: found.Emp_ID,
+                    Image:found.Image,
+                    FirstName:found.FirstName,
+                    LastName:found.LastName,
+                    Email_ID:found.Email_ID,
+                    DOB:found.DOB,
+                    Blood:found.Blood,
+                    Sex:found.Sex,
+                    Age:found.Age,
+                    phn_no:found.phn_no
+                })
+                }
+                
+            }
+        })
+    }else{
+        res.send("Scan the QR")
+    }
+    
+})
+
+
+
+
+app.listen(3002, function(){
+    console.log("Server started on port 3000")
+});
+
---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+// preloder
+
+var loader=document.getElementById("preloader");
+window.addEventListener("load",function(){
+    loader.style.display="none";
+})
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+
+
+/**
+ * HEADER
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+
